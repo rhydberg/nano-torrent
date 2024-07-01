912dfef676badb520b7715ae76dcf8e46ddcf13c
Ridin Datta <ridindatta12@gmail.com>
2024-07-02 02:57:18 +0530

    implemented bitfield methods

---
+++ bitfield/bitfield.go
@@ -0,0 +1,24 @@
+package bitfield
+
+type Bitfield []byte
+
+func (bf Bitfield) HasPiece(index int) bool{
+	byteIndex := index/8;
+	offset := index%8;
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
+
+	return bf[byteIndex]>>uint(7-offset)&1 != 0 //big endian
+}
+
+func (bf Bitfield) SetPiece(index int) {
+	byteIndex := index / 8
+	offset := index % 8
+
+	// silently discard invalid bounded index
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
+	bf[byteIndex] |= 1 << uint(7 - offset)
+}
