00d22397ad90e2174ca684d70246fed7601e23d3
Ridin Datta <ridindatta12@gmail.com>
2024-07-01 23:35:07 +0530

    implemented reading a serialized handshake - not tested

--- handshake/handshake.go
+++ handshake/handshake.go
@@ -1,31 +1,34 @@
 package handshake
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // import "github.com/rhydberg/gotorrent/torrentfile"
 
 type Handshake struct {
-	Length   byte
-	Pstr 	string
+	Length        byte
+	Pstr          string
 	ReservedBytes [8]byte
-	InfoHash [20]byte
-	PeerID [20]byte
+	InfoHash      [20]byte
+	PeerID        [20]byte
 }
 
-func New(infohash, peerID [20]byte) *Handshake{
-	pstr:="BitTorrent protocol"
+func New(infohash, peerID [20]byte) *Handshake {
+	pstr := "BitTorrent protocol"
 	handshake := Handshake{
-		Length: byte(len(pstr)),
-		Pstr: pstr,
+		Length:        byte(len(pstr)),
+		Pstr:          pstr,
 		ReservedBytes: [8]byte{},
-		InfoHash: infohash,
-		PeerID: peerID,
+		InfoHash:      infohash,
+		PeerID:        peerID,
 	}
 
 	return &handshake
 }
 
-func (h *Handshake) Serialize() []byte{
+func (h *Handshake) Serialize() []byte {
 	var buf []byte
 	buf = append(buf, h.Length)
 	buf = append(buf, []byte(h.Pstr)...)
@@ -36,6 +39,50 @@ func (h *Handshake) Serialize() []byte{
 	return buf
 }
 
-func ReadHandshake(r io.Reader) (*Handshake, error){
-	
+func ReadHandshake(r io.Reader) (*Handshake, error) {
+	lengthBuf := make([]byte, 1)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	pStrLen := int(lengthBuf[0])
+	if pStrLen == 0 {
+		err := fmt.Errorf("pstrlen cannot be 0")
+		return nil, err
+	}
+
+	pStrBuf := make([]byte, pStrLen)
+	_, err = io.ReadFull(r, pStrBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	reservedBytesBuf := make([]byte, 8)
+	_, err = io.ReadFull(r, reservedBytesBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	infoHashBuf := make([]byte, 20)
+	_, err = io.ReadFull(r, infoHashBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	peerIDBuf := make([]byte, 20)
+	_, err = io.ReadFull(r, peerIDBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	handshake := Handshake{
+		Length:        lengthBuf[0],
+		Pstr:          string(pStrBuf),
+		ReservedBytes: [8]byte(reservedBytesBuf),
+		InfoHash:      [20]byte(infoHashBuf),
+		PeerID:        [20]byte(peerIDBuf),
+	}
+
+	return &handshake, nil
 }
