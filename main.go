deb342de33652795a6fd2ce6e57f98a99f8ef611
Ridin Datta <ridindatta12@gmail.com>
2024-07-02 20:20:17 +0530

    implemented parsing of piece and have messages

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
-import (
-	// "fmt"
-	"github.com/rhydberg/gotorrent/torrentfile"
-	"log"
-)
+import "fmt"
+
+// "fmt"
+// "github.com/rhydberg/gotorrent/torrentfile"
+// "log"
+
+func fn(s []int){
+	s = append(s[:0], 2,3)
+}
+
 
 func main() {
-	path := "debian-12.5.0-amd64-netinst.iso.torrent"
+	// path := "debian-12.5.0-amd64-netinst.iso.torrent"
 
-	tf, err := torrentfile.GetTorrentFile(path)
+	// tf, err := torrentfile.GetTorrentFile(path)
 
-	if err != nil {
-		log.Fatalf("Error parsing torrent file")
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error parsing torrent file")
+	// }
 
-	// url, _ := tf.buildTrackerURL()
-	// fmt.Printf("%v", url)
-	tf.RequestPeers()
+	// // url, _ := tf.buildTrackerURL()
+	// // fmt.Printf("%v", url)
+	// tf.RequestPeers()
+
+	slice := make([]int, 3)
+	fmt.Printf("%v", slice)
+	fn(slice)
+	fmt.Printf("%v", slice)
 }
--- message/message.go
+++ message/message.go
@@ -3,6 +3,7 @@ package message
 import (
 	"encoding/binary"
 	"io"
+	"fmt"
 )
 
 
@@ -110,6 +111,85 @@ func CreateHaveMessage(index int) *Message{
 	}
 
 	return &message
+}
+
+func ParsePiece(index int, buf []byte, msg *Message) (int, error){
+	if msg.ID != MsgPiece {
+		return 0, fmt.Errorf("expected PIECE (ID %d), got ID %d", MsgPiece, msg.ID)
+	}
+
+	if len(msg.Payload) < 8{
+		return 0, fmt.Errorf("payload too short. %d < 8", len(msg.Payload))
+	}
+
+	parsedIndex:= binary.BigEndian.Uint32(msg.Payload[0:4])
+
+	if parsedIndex!= uint32(index){
+		return 0, fmt.Errorf("expected index %d, got %d", index, parsedIndex)
+	}
+	
+	begin:=binary.BigEndian.Uint32(msg.Payload[4:8])
+	if begin >= uint32(len(buf)) {
+		return 0, fmt.Errorf("begin offset too high. %d >= %d", begin, len(buf))
+	}
+
+	data := msg.Payload[8:]
+	if begin+uint32(len(buf)) > uint32(len(buf)) {
+		return 0, fmt.Errorf("data too long [%d] for offset %d with length %d", len(data), begin, len(buf))
+	}
+	copy(buf[begin:], data)
+	return len(data), nil
+}
+
+func ParseHave(msg *Message) (int, error){
+	if msg.ID != MsgHave {
+		return -1, fmt.Errorf("expected HAVE (ID %d), got ID %d", MsgHave, msg.ID)
+	}
+	if len(msg.Payload) != 4 {
+		return -1, fmt.Errorf("expected payload length 4, got length %d", len(msg.Payload))
+	}
+	
+	index := int(binary.BigEndian.Uint32(msg.Payload[:]))
+
+	return index, nil
+}
+
+
+
+
+func (m *Message) name() string {
+	if m == nil {
+		return "KeepAlive"
+	}
+	switch m.ID {
+	case MsgChoke:
+		return "Choke"
+	case MsgUnchoke:
+		return "Unchoke"
+	case MsgInterested:
+		return "Interested"
+	case MsgNotInterested:
+		return "NotInterested"
+	case MsgHave:
+		return "Have"
+	case MsgBitfield:
+		return "Bitfield"
+	case MsgRequest:
+		return "Request"
+	case MsgPiece:
+		return "Piece"
+	case MsgCancel:
+		return "Cancel"
+	default:
+		return fmt.Sprintf("Unknown#%d", m.ID)
+	}
+}
+
+func (m *Message) String() string {
+	if m == nil {
+		return m.name()
+	}
+	return fmt.Sprintf("%s [%d]", m.name(), len(m.Payload))
 }
 
 
